3126c8736f1f8753a39a203948d73feb91f3efd0
yeferson aroca <aroca.yefer@gmail.com>
2020-11-29 19:18:50 -0500

    iprueba 2.0

---
+++ hola.php
@@ -0,0 +1,14 @@
+u<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=s, initial-scale=1.0">
+    <title>Prueba1.0</title>
+</head>
+<body>
+psadffladalkmds
+    <label for="">hola</label>
+</body>
+</html>
+?>
